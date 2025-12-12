b7fb0e1a6d820870d31bba7dac8c3f318b6b2b7d
hermeswang <hermesw714eb@gmail.com>
2025-12-12 22:18:23 +0800

    測試

--- C_Version/main.c
+++ C_Version/main.c
@@ -18,11 +18,11 @@ Author:
 #include "struct.h"
 
 const EnemyTemplate enemy_db[] = {
-    {""    , -1, -1, -1, -1, -1}, // type 0 是 placeholder, 不應該存取到這
-    {"Slime"   , 20, 3, 4, 0, 0}, // type 1 是 Slime
+    {"", -1, -1, -1, -1, -1},     // type 0 是 placeholder, 不應該存取到這
+    {"Slime", 20, 3, 4, 0, 0},    // type 1 是 Slime
     {"Skeleton", 15, 8, 7, 5, 5}, // type 2 是 Skeleton
-    {"Zombie"  , 40, 5, 2, 1, 1}, // type 3 是 Zombie
-    {"Goblin"  , 30, 6, 6, 3, 3}, // type 4 是 Goblin
+    {"Zombie", 40, 5, 2, 1, 1},   // type 3 是 Zombie
+    {"Goblin", 30, 6, 6, 3, 3},   // type 4 是 Goblin
     {"Ghost", 5, 15, 10, 30, 10}  // type 5 是 Ghost
 };
 
@@ -48,30 +48,34 @@ ENEMY:
 */
 
 // 最多 玩家 + 5個敵人 = 6個實體，我開[8]的陣列確保不會出問題
-Entity entity[MAX_ENTITIES]; 
+Entity entity[MAX_ENTITIES];
 
 // player's backpack
-Backpack backpack; 
+Backpack backpack;
 
 int main()
 {
     srand((unsigned)time(NULL)); // 用當前時間初始化隨機數(確保一定程度的偽隨機)
 
-    int enemy_count = rand()%3 + 3; // randomly encounter 3~5 enemies, the player will fight them one by one
+    int enemy_count = rand() % 3 + 3;  // randomly encounter 3~5 enemies, the player will fight them one by one
     int total_count = enemy_count + 1; // entity[0] is player
 
     printf("==========================================\n");
     printf("Welcome to Raw RPG\n");
     printf("Enter your name (accept spaces, limit 31 characters): ");
-    if(fgets(entity[0].name, sizeof(entity[0].name), stdin) != NULL){
+    if (fgets(entity[0].name, sizeof(entity[0].name), stdin) != NULL)
+    {
         size_t len = strlen(entity[0].name);
-        
+
         // case A: user's input is correct, then we replace '\n' with '\0'
-        if(len > 0 && entity[0].name[len-1] == '\n') entity[0].name[len-1] = '\0';
+        if (len > 0 && entity[0].name[len - 1] == '\n')
+            entity[0].name[len - 1] = '\0';
         // case B: user's input is more thant 19 characters, we need to clear the input buffer
-        else{
+        else
+        {
             int c;
-            while((c = getchar()) != '\n' && c != EOF); // clear buffer
+            while ((c = getchar()) != '\n' && c != EOF)
+                ; // clear buffer
             // we don't need to manually replace the last character to '\n', it's automaticly done by fgets
         }
     }
@@ -85,10 +89,11 @@ int main()
     entity[0].dodge_rate = 10;
     entity[0].crit_rate = 5;
     entity[0].is_alive = 1;
-    
-    printf("==========================================\n");
+
+    printf("==========================================for realraeal\n");
     printf("Enemy list (3~5) for this game :\n");
-    for(int i=1; i<=enemy_count; ++i){
+    for (int i = 1; i <= enemy_count; ++i)
+    {
         setupEnemy(i);
     }
     printf("==========================================\n");
@@ -105,25 +110,28 @@ int main()
     backpack.gold = 0;
 
     printf("ID sequence before heap sort: ");
-    for(int i=0; i<backpack.item_count; i++) printf("%d ", backpack.items[i].id);
+    for (int i = 0; i < backpack.item_count; i++)
+        printf("%d ", backpack.items[i].id);
     printf("\n");
 
     sort_backpack(); // 執行排序
 
     printf("ID sequence after heap sort: ");
-    for(int i=0; i<backpack.item_count; i++) printf("%d ", backpack.items[i].id);
+    for (int i = 0; i < backpack.item_count; i++)
+        printf("%d ", backpack.items[i].id);
     printf("\n");
 
     return 0;
 }
 
 // 設定敵人屬性
-void setupEnemy(int i){
-    int type = rand()%5 + 1; // type依照 敵人清單.txt，敵人強度依照清單的順序
-    
+void setupEnemy(int i)
+{
+    int type = rand() % 5 + 1; // type依照 敵人清單.txt，敵人強度依照清單的順序
+
     // status from template
     strncpy(entity[i].name, enemy_db[type].name, sizeof(entity[i].name) - 1); // 存到destination的容量-1
-    entity[i].name[sizeof(entity[i].name) - 1] = '\0'; // 讓destination的最後一個位元變成 '\0'，保證字串安全
+    entity[i].name[sizeof(entity[i].name) - 1] = '\0';                        // 讓destination的最後一個位元變成 '\0'，保證字串安全
 
     entity[i].max_hp = enemy_db[type].hp;
     entity[i].atk = enemy_db[type].atk;
@@ -140,8 +148,10 @@ void setupEnemy(int i){
 }
 
 // 處理攻擊時的過程
-void execute_attack(Entity* entity1, Entity* entity2){
-    if(!entity1->is_alive || !entity2->is_alive) return; // if one of the entity is not alive anymore, don't do anything
+void execute_attack(Entity *entity1, Entity *entity2)
+{
+    if (!entity1->is_alive || !entity2->is_alive)
+        return; // if one of the entity is not alive anymore, don't do anything
 
     printf("%s attacked %s!\n", entity1->name, entity2->name);
 
@@ -182,41 +192,52 @@ the function call will be like:
 */
 
 // 骰防禦比例
-int roll_defend(){
+int roll_defend()
+{
     // TODO: if percent == 100, we need to give entity some awards, for example healing or attack bonus
     int percent = rand() % 100;
-    if(percent >= 50) return 0;  // 完美格檔 (受傷 0%)
-    if(percent >= 25) return 50; // 成功格檔 (受傷 50%)
-    if(percent >= 10) return 80; // 輕微格檔 (受傷 80%)
-    return 100; // 格檔失敗 (受傷 100%)
+    if (percent >= 50)
+        return 0; // 完美格檔 (受傷 0%)
+    if (percent >= 25)
+        return 50; // 成功格檔 (受傷 50%)
+    if (percent >= 10)
+        return 80; // 輕微格檔 (受傷 80%)
+    return 100;    // 格檔失敗 (受傷 100%)
 }
 
-void sort_backpack(){
+void sort_backpack()
+{
     int n = backpack.item_count;
-    for(int i=n/2-1; i>=0; --i){
+    for (int i = n / 2 - 1; i >= 0; --i)
+    {
         heapify_item(backpack.items, n, i);
     }
-    for(int i=n-1; i>=0; --i){
+    for (int i = n - 1; i >= 0; --i)
+    {
         swap_items(0, i);
         heapify_item(backpack.items, i, 0);
     }
 }
 
-void swap_items(int a, int b){
+void swap_items(int a, int b)
+{
     Item tmp = backpack.items[a];
     backpack.items[a] = backpack.items[b];
     backpack.items[b] = tmp;
 }
 
 // Max Heap, sort by id
-void heapify_item(Item arr[], int size, int i){
-    int l = i*2+1; // left child, right child = l+1
-    while(l < size){
-        int winner = (l+1 < size) && (arr[l+1].id > arr[l].id) ? l+1 : l;
+void heapify_item(Item arr[], int size, int i)
+{
+    int l = i * 2 + 1; // left child, right child = l+1
+    while (l < size)
+    {
+        int winner = (l + 1 < size) && (arr[l + 1].id > arr[l].id) ? l + 1 : l;
         winner = (arr[winner].id > arr[i].id) ? winner : i;
-        if(winner == i) break;
+        if (winner == i)
+            break;
         swap_items(i, winner);
         i = winner;
-        l = i*2+1;
+        l = i * 2 + 1;
     }
 }
